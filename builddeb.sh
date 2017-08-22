b71ce3e23b6948b2f00bae4f336a2f1cce065889
Vaibhav Singh <vaibhav1.s@samsung.com>
2017-08-22 12:27:48 +0200

    Add support for compiling with armhf debian
    Change-Id: I5280ac5b6b6a71dc3e3891ab431fe212a756b195
    Signed-off-by: Vaibhav Singh <vaibhav1.s@samsung.com>

--- builddeb.sh
+++ builddeb.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Please specify the target architecture like armhf, arm64 "
+	exit
+fi
+TARGET_ARCH=$1
+BUILD_DIR=build-$TARGET_ARCH
+echo "Tager Architecture is $TARGET_ARCH"
 echo "When cross-compiling, do not forget to set the environment variables CROSS_COMPILE, SYSROOT and CMAKE_PKG_CONFIG_PATH"
-
-rm -rf build/
-mkdir -p build/package/src/
+rm -rf $BUILD_DIR/
+mkdir -p $BUILD_DIR/package/src/
 git archive -o update.tgz HEAD
-mv update.tgz build/package/src/
-cd build/package/src/
+mv update.tgz $BUILD_DIR/package/src/
+cd $BUILD_DIR/package/src/
 tar -xvf update.tgz
-JOBS=$((2*$(nproc)))
-DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -j$JOBS -d -uc -us -aarm64 --target-arch arm64
+JOBS=$(nproc)
+DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -j$JOBS -d -uc -us -a $TARGET_ARCH --target-arch $TARGET_ARCH
